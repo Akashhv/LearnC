0d673359286488d8426f93b7a981627c8214961a
prithvihv <hvprithvi09@gmail.com>
2018-05-05 07:08:23 -0700

    First commit

---
+++ Calculator/test.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+int add(int num1,int num2){
+	
+}
+int sub(){
+
+}
+int mul(){
+
+}
+int div(){
+
+}
+int main(){
+	int option,a,b;
+	char gg[]="helloworld";
+	printf("Giff me what u want to do with ur life %s \n",gg);	
+	printf("1.Add\n2.Sub\n3.Mul\n4.Div\n");
+	scanf("%d",&option);
+	printf("Giff me 2 number :\n");
+	scanf("%d %d",&a,&b);
+	add(a,b);
+	add(5,6);
+	add(6,5);
+	switch(option){
+		case 1:add(a,b);
+	}
+
+	return 0;	
+}
